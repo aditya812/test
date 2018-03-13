ac52895baa0fb99b18ac090a167e04dc64e22a3a
Aditya <adityaa812@gmail.com>
2018-03-13 08:40:36 +0530

    master commit

--- newsHour/src/main/java/com/test/news/NewsHour.java
+++ newsHour/src/main/java/com/test/news/NewsHour.java
@@ -7,8 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NewsHour {
 
 	public static void main(String[] args) {
+		System.out.println("Master Commit");
 		// TODO Auto-generated method stub
 		SpringApplication.run(NewsHour.class, args);
+		System.out.println("Master Commit");
 	}
 
 }
